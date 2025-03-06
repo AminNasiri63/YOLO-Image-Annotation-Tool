b63ff2a2cbe383aec47b0ea7b88b32f3d7ef9fae
Amin Nasiri <aminnassiri63@gmail.com>
2025-03-06 12:05:59 -0500

    Labeling module

---
+++ LabelingModule/constants.py
@@ -0,0 +1,3 @@
+pad = 10
+alpha = 0.6
+process_end = False
---
+++ LabelingModule/modules.py
@@ -0,0 +1,9 @@
+import os
+import cv2
+import glob
+import tkinter as tk
+from pathlib import Path
+from tkinter import simpledialog, filedialog
+
+ROOT = tk.Tk()
+ROOT.withdraw()
